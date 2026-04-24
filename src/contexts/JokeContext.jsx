bd0080c121b89abc71a81d635da6116808cae8ad
abnerns <abnerdonantos06@gmail.com>
2026-04-24 17:00:33 -0300

    feat: implementar jokeContext com useReducer

--- src/contexts/JokeContext.jsx
+++ src/contexts/JokeContext.jsx
@@ -16,3 +16,50 @@ export const ACTIONS = {
   CLEAR_RESULTS: "CLEAR_RESULTS",
 };
 
+function jokeReducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.FETCH_START:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+        apiError: null,
+        lastSearch: action.payload,
+      };
+
+    case ACTIONS.FETCH_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        jokes: action.payload,
+        error: null,
+        apiError: null,
+      };
+
+    case ACTIONS.FETCH_ERROR:
+      return {
+        ...state,
+        loading: false,
+        jokes: [],
+        error: action.payload,
+      };
+
+    case ACTIONS.API_ERROR:
+      return {
+        ...state,
+        loading: false,
+        jokes: [],
+        apiError: action.payload,
+      };
+
+    case ACTIONS.CLEAR_RESULTS:
+      return {
+        ...initialState,
+      };
+
+    default:
+      return state;
+  }
+}
+
+
